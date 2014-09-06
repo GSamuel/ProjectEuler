34e6049b1bc32014b1315b845e12ecc9a5f705c7
GSamuel <gideon_hoog@hotmail.com>
2014-09-06 15:30:23 +0200

    problem 31 solved

---
+++ ProjectEuler/src/com/gshoogeveen/euler/solutions/Problem_31.java
@@ -0,0 +1,27 @@
+package com.gshoogeveen.euler.solutions;
+
+public class Problem_31
+{
+	private static int[] diffCoins = { 1, 2, 5, 10, 20, 50, 100, 200 };
+	private static int totalAmount = 0;
+	private static int max = 200;
+
+	public static void main(String[] args)
+	{
+		System.out.println(calculate(0, 0));
+	}
+
+	public static int calculate(int sum, int coinID)
+	{
+		if (sum == max)
+		{
+			return 1;
+		} else if (sum > max || coinID >= diffCoins.length)
+			return 0;
+		else
+		{
+			return (calculate(sum + diffCoins[coinID], coinID) + calculate(sum, coinID+1));
+			
+		}
+	}
+}
