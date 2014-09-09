136542ccda0426a30fde34f6a73fcbabfb2da421
GSamuel <gideon_hoog@hotmail.com>
2014-09-09 15:51:34 +0200

    solved problem 71

---
+++ ProjectEuler/src/com/gshoogeveen/euler/number/Fraction.java
@@ -0,0 +1,65 @@
+package com.gshoogeveen.euler.number;
+
+public class Fraction implements Comparable<Fraction>
+{
+	private int num, det;
+
+	public Fraction(int numerator, int determinator)
+	{
+		this.num = numerator;
+		this.det = determinator;
+	}
+
+	public int getNumerator()
+	{
+		return num;
+	}
+
+	public void setNumerator(int num)
+	{
+		this.num = num;
+	}
+
+	public int getDeterminator()
+	{
+		return det;
+	}
+
+	public void setDeterminator(int det)
+	{
+		this.det = det;
+	}
+
+	public double value()
+	{
+		return 1.0 * num / det;
+	}
+
+	@Override
+	public int compareTo(Fraction frac)
+	{
+		if (value() == frac.value())
+			return 0;
+		if (value() > frac.value())
+			return 1;
+		return -1;
+	}
+
+	public double difference(Fraction frac, boolean unsigned)
+	{
+		double num = difference(frac);
+		if (unsigned && num < 0)
+			num *= -1;
+		return num;
+	}
+
+	public double difference(Fraction frac)
+	{
+		return value() - frac.value();
+	}
+
+	public void show()
+	{
+		System.out.println(num + "/" + det + " : " + value());
+	}
+}
---
+++ ProjectEuler/src/com/gshoogeveen/euler/solutions/Problem_71.java
@@ -0,0 +1,34 @@
+package com.gshoogeveen.euler.solutions;
+
+import com.gshoogeveen.euler.number.Fraction;
+
+public class Problem_71
+{
+	//SOLVED//
+	public static void main(String[] args)
+	{
+		int max = 1000000;
+
+		Fraction goal = new Fraction(3, 7);
+		Fraction closest = new Fraction(1, 1);
+
+		Fraction test = new Fraction(1, 1);
+
+		while (test.getNumerator() <= max && test.getDeterminator() <= max)
+		{
+			if (goal.difference(closest, true) > goal.difference(test, true)
+					&& goal.difference(test) != 0)
+			{
+				closest.setNumerator(test.getNumerator());
+				closest.setDeterminator(test.getDeterminator());
+			}
+			
+			if (goal.difference(test) > 0)
+				test.setNumerator(test.getNumerator() + 1);
+			else
+				test.setDeterminator(test.getDeterminator() + 1);
+
+		}
+		closest.show();
+	}
+}
