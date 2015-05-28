6083c2ccbfc4c7f856021410017e04df3195aeec
Gideon <gideon_hoog@hotmail.com>
2015-05-28 15:23:07 +0200

    solved problem 63.

---
+++ ProjectEuler/src/com/gshoogeveen/euler/solutions/Problem_63.java
@@ -0,0 +1,25 @@
+package com.gshoogeveen.euler.solutions;
+
+import com.gshoogeveen.euler.number.IntegerCalculations;
+
+public class Problem_63 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		for(int a =1; a <100; a++)
+			for(int b = 1; b<100; b++)
+			{
+				int length = IntegerCalculations.power(a,b).toString().length();
+				if(b == length)
+					count++;
+			}
+		System.out.println("answer:"+count);
+
+	}
+
+}
