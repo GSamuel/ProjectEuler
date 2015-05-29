8e727f70120e8fcd55a28406d511f0f4be9c202e
GSamuel <gideon_hoog@hotmail.com>
2015-05-29 14:00:43 +0200

    solved problem 97

---
+++ ProjectEuler/src/com/gshoogeveen/euler/solutions/Problem_97.java
@@ -0,0 +1,17 @@
+package com.gshoogeveen.euler.solutions;
+
+import java.math.BigInteger;
+
+
+public class Problem_97
+{
+
+	public static void main(String[] args)
+	{
+		BigInteger bigNum = new BigInteger("28433").shiftLeft(7830457);
+		bigNum = bigNum.add(BigInteger.ONE);
+		
+		System.out.println("answer:" + bigNum.remainder(new BigInteger("10000000000")));
+	}
+
+}
